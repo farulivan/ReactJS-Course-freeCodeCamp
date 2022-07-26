93664793218c92b875cd050eb94a94c9201c7bdc
Farul Ivan <farulivan@gmail.com>
2022-07-26 16:30:42 +0700

    making an Intro folder

--- Project 1 - React info site/index.js
+++ Project 1 - React info site/index.js
@@ -1,7 +1,21 @@
 // import React from "react"
 // import ReactDOM from "react-dom"
 
+function MainContent() {
+    return (
+        <nav>
+            <h1>Let's practice making a Nav</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+
 ReactDOM.render(
-    <h1 classname="header">This is JSX</h1><p>This is a paragraph</p>,
-    document.getElementById('root')
+        <MainContent />,
+    document.getElementById("root")
 )
