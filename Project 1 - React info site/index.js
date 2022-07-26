7a6d843212420ba942829fdee669f997bdab4bcc
Farul Ivan <farulivan@gmail.com>
2022-07-26 15:20:41 +0700

    adding some basic structure

---
+++ Project 1 - React info site/index.js
@@ -0,0 +1,7 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <h1 classname="header">This is JSX</h1><p>This is a paragraph</p>,
+    document.getElementById('root')
+)
